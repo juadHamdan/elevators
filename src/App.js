77c43a03bad9ea607dd5e638fec10153f8023364
juadhamdan <joad.hamdan@gmail.com>
2023-03-08 20:56:03 +0200

    elevators and queue added to app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,107 @@
 import './App.css';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Elevator from './components/Elevator';
 
-function App() {
-  const [call, setCall] = useState(false)
-  
-  return (
+const NUM_OF_FLOORS = 10
+const NUM_OF_ELEVATORS = 5
+const ElevatorBtnClassNames = {
+  Arrived: "btn-active",
+  Traveling: "btn-disabled",
+  Default: "btn-default"
+}
+const ARRIVAL_TIMEOUT = 2000
+const initialElevatorsLocations = new Map([...Array(5).keys()].map(key => [key, 0]))
 
+function App() {
+  const [desiredFloor, setDesiredFloor] = useState(0)
+  const [elevatorChosen, setElevatorChosen] = useState(null)
+  const [btnClasnName, setBtnClassName] = useState(ElevatorBtnClassNames.Default)
+  const [callsQueue, setCallsQueue] = useState([])
+  const [elevatorsLocations, setElevatorsLocations] = useState(initialElevatorsLocations)
+
+  function getElevatorLabel(elevatorNum){
+    switch(elevatorNum){
+      case 0: 
+        return "Ground Floor"
+      case 1: 
+        return "1st"
+      default: 
+        return `${elevatorNum}th`
+    }
+  }
+
+  function updateElevatorLocation(elevatorNum, floorNum){
+    const nextElevatorsLocations = elevatorsLocations
+    nextElevatorsLocations.set(elevatorNum, floorNum)
+    setElevatorsLocations(elevatorsLocations)
+  }
+
+  function onElevatorReady(elevatorNum, floorNum){
+    setBtnClassName(ElevatorBtnClassNames.Default)
+    updateElevatorLocation(elevatorNum, floorNum)
+  }
+
+  //elevatorNum => elevatorIndex
+  function onElevatorArrival(elevatorNum, floorNum){
+    setBtnClassName(ElevatorBtnClassNames.Arrived)
+    setTimeout(() => {
+      onElevatorReady(elevatorNum, floorNum)
+    }, ARRIVAL_TIMEOUT)
+  }
+
+  function identifyClosestElevator(floorNum){
+    const sortedByClosestLocations = new Map([...elevatorsLocations.entries()].map(a => [a[0], Math.abs(a[1] - floorNum)]).sort((a, b) => a[1] - b[1]))
+    const closestElevatorNum = sortedByClosestLocations.entries().next().value[0]
+    return closestElevatorNum
+  }
+  
+
+
+  function onCall(floorNum){
+    // Add call to qeoue
+    console.log("Queoe:", [...callsQueue, floorNum])
+    setCallsQueue([...callsQueue, floorNum])
+
+    const elevatorNum = identifyClosestElevator(floorNum)
+    console.log("Elevator:", elevatorNum, "Floor:", floorNum)
+    updateElevatorLocation(elevatorNum, floorNum)
+    //startElevatorTravel(elevatorNum, floorNum)
+
+    // Remove call to qeoue
+    setCallsQueue(callsQueue.slice(1))
+  }
+
+  return (
     <div className="App">
-      <Elevator 
-        call={call}
-        numOfFloors={10} 
-        desiredFloor={2}
-      />
-      <button onClick={() => setCall(true)}>Call Second Floor</button>
+      <div className="elevators-labels">
+        {[...Array(NUM_OF_FLOORS).keys()].reverse().map(floorNum => 
+          <h3 key={floorNum} className="elevator-label">{getElevatorLabel(floorNum)}</h3>
+        )}
+      </div>
+
+      {[...Array(NUM_OF_ELEVATORS).keys()].map(elevatorNum => 
+        <Elevator 
+          key={elevatorNum}
+          numOfFloors={NUM_OF_FLOORS} 
+          desiredFloor={elevatorsLocations.get(elevatorNum)}
+          onArrival={(floorNum) => onElevatorArrival(elevatorNum, floorNum)}
+          onTraveling={() => setBtnClassName(ElevatorBtnClassNames.Traveling)}
+          timeoutOnArrival={ARRIVAL_TIMEOUT}
+        />
+      )}
+
+      <div className="elevators-controller">
+        {[...Array(NUM_OF_FLOORS).keys()].reverse().map(floorNum => 
+          <button 
+            key={floorNum}
+            className={`btn ${desiredFloor === floorNum ? btnClasnName : ElevatorBtnClassNames.Default}`}
+            onClick={() => onCall(floorNum)}>
+              {callsQueue}
+          </button>
+        )}
+
+      </div>
+
     </div>
   );
 }
--- src/components/Elevator.js
+++ src/components/Elevator.js
@@ -1,61 +1,99 @@
 import './elevator.css'
 import { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import {ReactComponent as ElevatorIcon} from './sources/elevator-icon.svg'
 
 const initialFloor = 0
-const floorWidthInPx = 120
-const floorHeightInPx = 60
-const floorTravelSpeedInSec = 1
+const floorWidthInPx = 140
+const floorHeightInPx = 75
+const floorTravelSpeedInSec = 5
+
+const ElevatorStatusColor = {
+    Arrived: "#66bb6a",
+    Traveling: "#f44336",
+    Default: "#404040"
+}
 
 const Elevator = ({
-    call,
     numOfFloors,
-    desiredFloor,
+    desiredFloor = 0,
+    onTraveling,
+    onArrival,
+    timeoutOnArrival
 }) => {
     const [currentFloor, setCurrentFloor] = useState(initialFloor)
-    const [floorsToTravel, setFloorsToTravel] = useState(Math.abs(desiredFloor - currentFloor))
-    const [isTraveling, setIsTraveling] = useState(false)
-    const [hasArrived, setHasArrived] = useState(false)
-    //const [isTravelingUp]
+    const [floorsToTravel, setFloorsToTravel] = useState(0)
+    const [elevetorIconColor, setElevetorIconColor] = useState(ElevatorStatusColor.Default)
+    const [startAnimation, setStartAnimation] = useState(false)
 
-    const floorSizeStyle = {
-        width: `${floorWidthInPx}px`,
-        height: `${floorHeightInPx}px`
+    const floorStyle = {
+        width: `${floorWidthInPx - 1}px`,
+        height: `${floorHeightInPx - 1}px`,
+        border: "1px solid #f5f5f5"
     }
 
-    function getTravelHeightPx(){
+    function getTravelHeightInPx(){
         return `${-(floorsToTravel * floorHeightInPx)}px`
     }
-    function getElevetorCurrentCSSColor(){
-        if(hasArrived){
-            return '#66bb6a'
-        }
-        if(isTraveling){
-            return '#f44336'
-        }
-        return 'black'
+
+    function getTransitionDurationInSec(){
+        return Math.abs(floorsToTravel * floorTravelSpeedInSec)
     }
-    const elevatorIconAnimationStyle = {
-        transform: `translateY(${call ? getTravelHeightPx() : ''})`,
-        transitionDuration: `${floorsToTravel}s`
+
+    function getElevatorIconAnimationStyle(){
+        if(startAnimation){
+            return {
+                transform: `translateY(${getTravelHeightInPx()})`,
+                transitionDuration: `${getTransitionDurationInSec()}s`}
+        }
+        return{
+            transform: `translateY(0)`,
+            transitionDuration: `0s`
+            }
     }
 
     useEffect(() => {
-        if(call){
-            setIsTraveling(true)
+        const hasElevatorCalled = desiredFloor !== currentFloor
+        if(hasElevatorCalled){
+            console.log("desiredFloor:", desiredFloor, "currentFloor:", currentFloor)
+            setFloorsToTravel(desiredFloor - currentFloor)
+
+
+            ////
+            setStartAnimation(true)
+
+
+
+            setElevetorIconColor(ElevatorStatusColor.Traveling)
+            onTraveling()
             setTimeout(() => {
-                setIsTraveling(false)
-                setHasArrived(true)
-            }, floorsToTravel * (floorTravelSpeedInSec * 1000))
+                setElevetorIconColor(ElevatorStatusColor.Arrived)
+
+
+                ///
+                setStartAnimation(false)
+
+
+
+                setCurrentFloor(desiredFloor)
+                onArrival(desiredFloor)
+                setTimeout(() => {
+                    setElevetorIconColor(ElevatorStatusColor.Default)
+                }, timeoutOnArrival)
+            }, Math.abs(floorsToTravel) * (floorTravelSpeedInSec * 1000))
         }
-    })
+    }, [desiredFloor])
 
     return(
         <div className="elevator-container">
             {[...Array(numOfFloors).keys()].reverse().map(index => 
-                <div key={index} className="floor" style={floorSizeStyle}>
+                <div key={index} className="floor" style={floorStyle}>
                     {index === currentFloor ?
-                        <ElevatorIcon stroke={getElevetorCurrentCSSColor()} className="elevator-icon" style={elevatorIconAnimationStyle}/>
+                        <ElevatorIcon 
+                            stroke={elevetorIconColor} 
+                            className="elevator-icon" 
+                            style={getElevatorIconAnimationStyle()}
+                        />
                     :  null}
                 </div>
             )}
